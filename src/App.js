2a9a9953812e0bb303492b9e3b5b4d2faf93446d
Luiz Henrique Barbosa <luizhenriquebarbosa21@gmail.com>
2024-10-08 19:52:19 -0300

    Desafio da Calculadora concluído

--- src/App.js
+++ src/App.js
@@ -49,6 +49,34 @@ const App = () => {
 
   }
 
+  const handleMultiplyNumbers = () => {
+
+    if(firstNumber === '0'){
+    setFirstNumber(String(currentNumber));
+    setCurrentNumber('0')
+    setOperation('-')
+  }else {
+    const sum = Number(firstNumber) * Number(currentNumber);
+    setCurrentNumber(String(sum))
+    setOperation('')
+  }
+
+  }
+
+  const handleDivideNumbers = () => {
+
+    if(firstNumber === '0' || currentNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('')
+    }else {
+      const sum = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+
+  }
+
   const handleEquals = () => {
 
     if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
@@ -71,8 +99,8 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x"/>
-          <Button label="/"/>
+          <Button label="x" onClick={handleMultiplyNumbers}/>
+          <Button label="/" onClick={handleDivideNumbers}/>
           <Button label="c" onClick={handleOnClear}/>
           <Button label="."/>
         </Row>
